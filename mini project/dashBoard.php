c371a344321a548b63f68e355ee5ff94ae67281b
Saharier203 <sayebsayeb2002@gmail.com>
2024-10-02 08:04:16 -0700

    Add files via upload

---
+++ mini project/dashBoard.php
@@ -0,0 +1,14 @@
+<html>
+    <head> 
+        <title>
+            Online Voting System - Dashboard
+        </title>
+    </head>
+    <body>
+        <button>Back</button>
+        <button>Logout</button>
+        <H1>Vote Your Candidate</H1>
+        <div id="Profile"></div>
+        <div id="group"></div>
+    </body>
+</html>
